77017815a861a10dcf75af4bcfaad95e79ddfdfb
Matthew Zenn <spyguy57@gmail.com>
2023-03-27 15:22:36 -0400

    initialized app

---
+++ logic.js
@@ -0,0 +1,21 @@
+var toggle = 0;
+
+document.getElementById('switch').addEventListener('click', () => {
+    if(toggle == 0) {
+        toggle = 1;
+        themeSwitch();
+    }
+    else {
+        toggle = 0;
+        themeSwitch();
+    }
+});
+
+function themeSwitch()  {
+    if (toggle == 0) {
+        document.documentElement.setAttribute('data-theme', 'root');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+}
